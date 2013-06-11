f81b0916b3748993b79aed77ad21eb2296ec1b44
Arvid Waldner <arvid.waldner87@gmail.com>
2013-06-11 17:11:48 +0200

    First version checkin

---
+++ MyNotepad/EditorPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace MyNotepad
+{
+    public partial class EditorPage : PhoneApplicationPage
+    {
+        public EditorPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Debug.WriteLine(App.CurrentFileName);
+
+            if (App.CurrentFileName != null)
+            {
+                Header.Text = "edit";
+                var storage = IsolatedStorageFile.GetUserStoreForApplication();
+                using (var stream = storage.OpenFile(App.CurrentFileName, FileMode.Open))
+                {
+                    StreamReader reader = new StreamReader(stream);
+                    var note = reader.ReadToEnd();
+                    Editor.Text = note;
+                }
+                
+                
+            }
+        }
+
+        private void ApplicationBarIconButton_Click_1(object sender, EventArgs e)
+        {
+            // So far, we assume that all files are new ones!
+            // 1. Name the file from the first line of text
+            var firstLinefeed = Editor.Text.IndexOfAny(new char[] { '\r', '\n' });
+            string filename;
+
+            if (firstLinefeed > -1)
+            {
+                filename = Editor.Text.Substring(0, firstLinefeed);
+            }
+            else
+            {
+                filename = Editor.Text;
+            }
+
+            // Remove non-alphanumeric chars from filename
+            filename = new Regex("[^0-9a-zA-Z]").Replace(filename, string.Empty);
+            filename = "/" + filename + ".txt";
+
+            Debug.WriteLine(filename);
+
+            // 2. Save to isolated storage
+            var storage = IsolatedStorageFile.GetUserStoreForApplication();
+            using (var stream = storage.CreateFile(filename))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write(Editor.Text);
+                    writer.Flush();
+                }
+            }
+        }
+    }
+}
---
+++ MyNotepad/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace MyNotepad
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public class NoteDisplayModel
+        {
+            public string Title { get; set; }
+            public string Filename { get; set; }
+        }
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            RefillFilenamesList();
+        }
+
+        private void RefillFilenamesList()
+        {
+            var storage = IsolatedStorageFile.GetUserStoreForApplication();
+
+            var filenames = storage.GetFileNames("*.txt");
+
+            var nicerFilenames = from filename in filenames
+                                 orderby filename
+                                 select new NoteDisplayModel
+                                 {
+                                     Title = System.IO.Path.GetFileNameWithoutExtension(filename),
+                                     Filename = filename
+                                 };
+
+            AllFiles.ItemsSource = nicerFilenames;
+        }
+
+        private void ApplicationBarIconButton_Click_1(object sender, EventArgs e)
+        {
+            string uri = "/EditorPage.xaml";
+            App.CurrentFileName = null;
+
+            NavigationService.Navigate(new Uri(uri, UriKind.Relative));
+            
+        }
+
+        private void AllFiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                var item = (NoteDisplayModel)e.AddedItems[0];
+                var filename = item.Filename;
+
+                App.CurrentFileName = filename;
+                NavigationService.Navigate(new Uri("/EditorPage.xaml", UriKind.Relative));
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            var filename = (string)((MenuItem)sender).CommandParameter;
+
+            var storage = IsolatedStorageFile.GetUserStoreForApplication();
+            storage.DeleteFile(filename);
+
+            RefillFilenamesList();
+        }
+    }
+}
